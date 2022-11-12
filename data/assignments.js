4309932384115cc5aed020113571902683c07913
planit <planit@tanjeffreyz.github.io>
2022-11-12 04:51:28 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -376,7 +376,7 @@ assignments = {
     {
       "title": "[GRADED] Project 5",
       "course": "CS 186",
-      "dueDate": "2022-11-22T23:59:00",
+      "dueDate": "2022-11-27T23:59:00",
       "link": "https://www.gradescope.com/courses/414436",
       "submitted": false
     },
