6c103345c26dc8a3ef053835481aeec7b6bdb467
planit <planit@tanjeffreyz.github.io>
2022-11-14 09:39:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -360,6 +360,13 @@ assignments = {
       "submitted": true
     },
     {
+      "title": "[GRADED] Vitamin 12: Distributed Transactions",
+      "course": "CS 186",
+      "dueDate": "2022-11-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/414436",
+      "submitted": false
+    },
+    {
       "title": "[GRADED] Project 4 Part 1",
       "course": "CS 186",
       "dueDate": "2022-10-28T23:59:00",
