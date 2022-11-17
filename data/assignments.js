2674251ba416a4d6948fe8fedfe9320788888a0b
planit <planit@tanjeffreyz.github.io>
2022-11-17 06:43:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -300,8 +300,8 @@ assignments = {
       "title": "[GRADED] Vitamin 11: Parallel Query Processing",
       "course": "CS 186",
       "dueDate": "2022-11-16T23:59:00",
-      "link": "https://www.gradescope.com/courses/414436/assignments/2153856/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/414436/assignments/2153856/submissions/149443399",
+      "submitted": true
     },
     {
       "title": "[GRADED] Vitamin 9: Recovery",
