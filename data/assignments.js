160c08b930144eb58458f6861c5c46ace4cace87
planit <planit@tanjeffreyz.github.io>
2022-11-21 16:52:11 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -370,7 +370,7 @@ assignments = {
       "title": "[GRADED] Vitamin 13: NoSQL",
       "course": "CS 186",
       "dueDate": "2022-11-30T23:59:00",
-      "link": "https://www.gradescope.com/courses/414436",
+      "link": "https://www.gradescope.com/courses/414436/assignments/2180573/submissions/new",
       "submitted": false
     },
     {
