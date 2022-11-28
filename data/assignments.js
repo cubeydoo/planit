60c45b74ce1d63de7f4250612b275cb2fbe352b6
planit <planit@tanjeffreyz.github.io>
2022-11-28 04:41:05 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -398,8 +398,8 @@ assignments = {
       "title": "[GRADED] Project 5",
       "course": "CS 186",
       "dueDate": "2022-11-27T23:59:00",
-      "link": "https://www.gradescope.com/courses/414436",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/414436/assignments/2180577/submissions/150972849",
+      "submitted": true
     },
     {
       "title": "[GRADED] Project 6",
