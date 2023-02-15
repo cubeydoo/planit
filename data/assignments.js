811fe6333e79164133c644a00242ee146065ee5b
planit <planit@tanjeffreyz.github.io>
2023-02-15 04:37:31 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,7 +1,7 @@
 assignments = {
   "CS 184/284A": [
     {
-      "title": "HW 1 Checkpoint",
+      "title": "Project 1 Checkpoint",
       "course": "CS 184/284A",
       "dueDate": "2023-02-07T23:59:00",
       "link": "https://www.gradescope.com/courses/483806/assignments/2596089/submissions/160025339",
@@ -11,11 +11,11 @@ assignments = {
       "title": "Project 1: Rasterizer",
       "course": "CS 184/284A",
       "dueDate": "2023-02-14T23:59:00",
-      "link": "https://www.gradescope.com/courses/483806",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/483806/assignments/2627156/submissions/162041095",
+      "submitted": true
     },
     {
-      "title": "HW 2 Checkpoint",
+      "title": "Project 2 Checkpoint",
       "course": "CS 184/284A",
       "dueDate": "2023-02-21T23:59:00",
       "link": "https://www.gradescope.com/courses/483806/assignments/2658241/submissions/new",
@@ -25,8 +25,8 @@ assignments = {
       "title": "Project 2: MeshEdit",
       "course": "CS 184/284A",
       "dueDate": "2023-02-28T23:59:00",
-      "link": "https://www.gradescope.com/courses/483806",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/483806/assignments/2659849/submissions/162040133",
+      "submitted": true
     }
   ]
 };
