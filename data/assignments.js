223fd081bc4abfa0a14641d6120b11238f200742
planit <planit@tanjeffreyz.github.io>
2023-06-11 16:35:06 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,1 +1,151 @@
-assignments = {};
+assignments = {
+  "CS 184/284A": [
+    {
+      "title": "Project 1 Checkpoint",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2596089/submissions/160025339",
+      "submitted": true
+    },
+    {
+      "title": "Project 1: Rasterizer",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2627156/submissions/162041095",
+      "submitted": true
+    },
+    {
+      "title": "Project 2 Checkpoint",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2658241/submissions/163624750",
+      "submitted": true
+    },
+    {
+      "title": "Project 2: MeshEdit",
+      "course": "CS 184/284A",
+      "dueDate": "2023-02-28T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2659849/submissions/165444171",
+      "submitted": true
+    },
+    {
+      "title": "Participation Tracker",
+      "course": "CS 184/284A",
+      "dueDate": "2023-05-31T08:00:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2665249/submissions/163892965",
+      "submitted": true
+    },
+    {
+      "title": "Project 3-1 Checkpoint",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2688745/submissions/167440329",
+      "submitted": true
+    },
+    {
+      "title": "Project 3-2 Checkpoint",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2691587/submissions/169852159",
+      "submitted": true
+    },
+    {
+      "title": "Project 4 Checkpoint",
+      "course": "CS 184/284A",
+      "dueDate": "2023-04-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2691591/submissions/172645274",
+      "submitted": true
+    },
+    {
+      "title": "Project 3-1: Pathtracer",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2691593/submissions/168593097",
+      "submitted": true
+    },
+    {
+      "title": "Project 4: ClothSim",
+      "course": "CS 184/284A",
+      "dueDate": "2023-04-11T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2691651/submissions/174383744",
+      "submitted": true
+    },
+    {
+      "title": "Exam 1",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2707953/submissions/167082692",
+      "submitted": true
+    },
+    {
+      "title": "Practice Exam 1",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-06T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2721966/submissions/166877358",
+      "submitted": true
+    },
+    {
+      "title": "Exam 1, 1.5x",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    },
+    {
+      "title": "Exam 1, 2x",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-07T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    },
+    {
+      "title": "Project 3-2: Pathtracer",
+      "course": "CS 184/284A",
+      "dueDate": "2023-03-24T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2757546/submissions/170978301",
+      "submitted": true
+    },
+    {
+      "title": "Practice Exam 2 - Full Length (SP22)",
+      "course": "CS 184/284A",
+      "dueDate": "2023-04-20T00:00:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    },
+    {
+      "title": "Practice Exam 2",
+      "course": "CS 184/284A",
+      "dueDate": "2023-04-20T00:00:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    },
+    {
+      "title": "Exam 2",
+      "course": "CS 184/284A",
+      "dueDate": "2023-04-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2837537/submissions/176189665",
+      "submitted": true
+    },
+    {
+      "title": "Exam 2 1.5x",
+      "course": "CS 184/284A",
+      "dueDate": "2023-04-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    },
+    {
+      "title": "Exam 2 2x",
+      "course": "CS 184/284A",
+      "dueDate": "2023-04-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806",
+      "submitted": false
+    },
+    {
+      "title": "Grade Report",
+      "course": "CS 184/284A",
+      "dueDate": "2023-05-12T23:59:00",
+      "link": "https://www.gradescope.com/courses/483806/assignments/2879556/submissions/179828057",
+      "submitted": true
+    }
+  ]
+};
